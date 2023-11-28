6b3c945e553f8168a2da48c7229cf2230f2156ea
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-11-28 09:28:16 -0600

    Initial Commit

---
+++ Coin Flip/Coin_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_FlipApp.swift
+//  Coin Flip
+//
+//  Created by chase Hashiguchi on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Coin_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
