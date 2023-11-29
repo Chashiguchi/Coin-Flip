1605ac109b0f7c98b168434793c7aea9d1fe10bd
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-11-29 09:41:14 -0600

    Added Image and Rotation effects

--- Coin Flip/ContentView.swift
+++ Coin Flip/ContentView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rotation = 0.0
+    @State private var randomValue = 0
     var body: some View {
         VStack {
             CustomText(text: "Coin Flip")
+            Image("Coin")
+                .resizable()
+                .frame(width:200, height:200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 0, z: 0))
+                .onTapGesture {
+                    chooseRandom(times: 10)
+                }
         }
         .padding()
+    }
+    func chooseRandom(times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+                randomValue = Int.random(in: 1...12)
+                chooseRandom(times: -1)
+                withAnimation(.interpolatingSpring(stiffness: 5, damping: 2)) {
+                    rotation += 360
+                }
+            }
+        }
     }
 }
 
