53b1008f3c31960a502206da186a4d5132a6e116
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-12-14 09:55:03 -0600

    Fixed animation

--- Coin Flip/ContentView.swift
+++ Coin Flip/ContentView.swift
@@ -9,28 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rotation = 0.0
-    @State private var randomValue = 0
+    @State private var coinSide = "Heads"
+    @State private var isFlipped = false
     var body: some View {
         VStack {
-            CustomText(text: "Coin Flip")
-            Image("Coin")
+            Image("Coin 1")
                 .resizable()
-                .frame(width:200, height:200, alignment: .center)
-                .rotationEffect(.degrees(rotation))
-                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 0, z: 0))
-                .onTapGesture {
-                    chooseRandom(times: 10)
-                }
+                .frame(width: 150, height: 150, alignment: .center)
+            CustomText(text: "Coin Flip")
+                .padding(20)
+            Image("\(coinSide)")
+                .resizable()
+                .frame(width: 150, height: 150, alignment: .center)
+                .rotation3DEffect(.degrees((rotation)), axis: (x: 1, y: 0, z: 0))
+                .animation(.easeInOut(duration: 0.5))
+            Button("Flip Coin") {
+                coinSide = Bool.random() ? "Heads" : "Tails"
+                chooseRandom(times: 2)
+                isFlipped.toggle()
+            }
         }
         .padding()
     }
     func chooseRandom(times: Int) {
         if times > 0 {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1){
-                randomValue = Int.random(in: 1...12)
                 chooseRandom(times: -1)
-                withAnimation(.interpolatingSpring(stiffness: 5, damping: 2)) {
-                    rotation += 360
+                withAnimation(.interpolatingSpring(stiffness: 2, damping: 5)) {
+                    if Bool.random() {
+                        coinSide = "Heads"
+                    } else {
+                        coinSide = "Tails"
+                    }
+                    rotation += 3600
                 }
             }
         }
