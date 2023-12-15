29e2d3ba1a6e78a18e3a1407c150bd9856863220
Chase Hashiguchi <chashiguchi@bsd220.org>
2023-12-15 09:54:53 -0600

    added comments

--- Coin Flip/ContentView.swift
+++ Coin Flip/ContentView.swift
@@ -9,28 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rotation = 0.0
+    //chooses coin side make sure the coinn isn't flipped
     @State private var coinSide = "Heads"
     @State private var isFlipped = false
     var body: some View {
         VStack {
-            Image("Coin 1")
+            //Logo
+            Image("Heads")
                 .resizable()
-                .frame(width: 150, height: 150, alignment: .center)
+                .frame(width: 100, height: 100, alignment: .center)
             CustomText(text: "Coin Flip")
-                .padding(20)
+                .padding(50)
+            //Chooses random side and flips coin
             Image("\(coinSide)")
                 .resizable()
                 .frame(width: 150, height: 150, alignment: .center)
                 .rotation3DEffect(.degrees((rotation)), axis: (x: 1, y: 0, z: 0))
                 .animation(.easeInOut(duration: 0.5))
+                .padding(20)
+            //Button to flip coin
             Button("Flip Coin") {
                 coinSide = Bool.random() ? "Heads" : "Tails"
                 chooseRandom(times: 2)
                 isFlipped.toggle()
             }
+            .foregroundColor(.black)
+            Spacer()
         }
         .padding()
     }
+    //chooses random side of the coin
     func chooseRandom(times: Int) {
         if times > 0 {
             DispatchQueue.main.asyncAfter(deadline: .now() + 1){
@@ -53,7 +61,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
+// makes custom text
 struct CustomText: View {
     let text: String
     var body: some View{
